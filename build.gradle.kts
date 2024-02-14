a8be233886cbb6d3c2ff792bd94fadaeb8520b02
Innocento <omidoinnocent32@gmail.com>
2024-02-14 22:57:56 +0300

    added more dependencies: chromedriver, testNG, cucumber

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,13 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java
     implementation("org.seleniumhq.selenium:selenium-java:4.17.0")
+    // https://mvnrepository.com/artifact/org.testng/testng
+    testImplementation("org.testng:testng:7.9.0")
+    // https://mvnrepository.com/artifact/io.cucumber/cucumber-java
+    implementation("io.cucumber:cucumber-java:7.15.0")
+    implementation("io.github.bonigarcia:webdrivermanager:5.6.3")
+
+
 
 }
 
